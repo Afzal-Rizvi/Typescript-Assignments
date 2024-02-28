61478b66ae8885296ba1bbec26d197f0054c3649
Afzal-Rizvi <161647109+Afzal-Rizvi@users.noreply.github.com>
2024-02-29 02:00:12 +0500

    Add files via upload

---
+++ 12 Greetings_Names.ts
@@ -0,0 +1,9 @@
+
+
+let names :string[] = ['Afzal Rizvi', 'Hilal', 'Hamza', 'Mujtaba'];
+
+const message :string = 'Welcome to Typescript Class: '
+
+for(let i=0; i<names.length; i++) {
+    console.log (message + names [i]);
+}
